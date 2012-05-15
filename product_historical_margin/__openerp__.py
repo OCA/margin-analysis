98f2159f39fcc3b2e206b219846f65a17fba29a7
Alexandre Fayolle @ camptocamp <alexandre.fayolle@camptocamp.com>
2012-05-15 18:04:34 +0200

    [WIP] product_historical_margin: skeleton (lp:c2c-addons/6.1  rev 40.1.20)

---
+++ product_historical_margin/__openerp__.py
@@ -0,0 +1,41 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+#
+#    Author:  Alexandre Fayolle
+#    Copyright 2012 Camptocamp SA
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+{'name' : 'Product Historical Margin',
+ 'version' : '0.1',
+ 'author' : 'Camptocamp',
+ 'maintainer': 'Camptocamp',
+ 'category': 'Hidden',
+ 'complexity': "normal",  # easy, normal, expert
+ 'depends' : ['product',
+              'account',
+              ],
+ 'description': """Provides an improved way of computing the margin and markup of a product, especially for product with a averaged cost price. XXX
+ """,
+ 'website': 'http://www.camptocamp.com/',
+ 'init_xml': [],
+ 'update_xml': [],
+ 'demo_xml': [],
+ 'tests': [],
+ 'installable': True,
+ 'auto_install': False,
+ 'license': 'AGPL-3',
+ 'application': False
+ }
