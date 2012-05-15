7f1ccc6fdd9570ea78dc3fa503bbb89ffa11dd7a
Alexandre Fayolle @ camptocamp <alexandre.fayolle@camptocamp.com>
2012-05-15 18:04:01 +0200

    [FIX] product_cost_incl_bom: proper fields.function prototype for get_cost + refactoring of _compute_purchase_price (lp:c2c-addons/6.1  rev 40.1.19)

--- product_cost_incl_bom/product_cost_incl_bom.py
+++ product_cost_incl_bom/product_cost_incl_bom.py
@@ -59,47 +59,44 @@ class Product(Model):
             #if not hasattr(pr, 'standard_price'): return False
             bom_id = bom_obj._bom_find(cursor, user, pr.id, product_uom=None, properties=bom_properties)
 
-            if bom_id:
-                bom = bom_obj.browse(cursor, user, bom_id)
+            if not bom_id: # no BoM: use standard_price
+                res[pr_id] = pr.standard_price
+                continue
 
-                sub_products, routes = bom_obj._bom_explode(cursor, user, bom,
-                                                            factor=1,
-                                                            properties=bom_properties,
-                                                            addthis=True)
-
-                res[pr.id] = 0.0
-                for sub_prod_dict in sub_products:
-                    sub_product = self.browse(cursor, user, sub_product_dict['product_id'])
-                    std_price = sub_product.standard_price
-                    qty = uom_obj._compute_qty(cursor, user,
-                                               from_uom_id = sub_product_dict['product_uom'],
-                                               qty         = sub_product_dict['product_qty'],
-                                               to_uom_id   = sub_product.uom_po_id.id)
-
-                    res[pr.id] += std_price * qty
-                    # TODO alf use routes to compute cost of manufacturing (following is from product_extended)
-                    ## if bom.routing_id:
-                    ##     for wline in bom.routing_id.workcenter_lines:
-                    ##         wc = wline.workcenter_id
-                    ##         cycle = wline.cycle_nbr
-                    ##         hour = (wc.time_start + wc.time_stop + cycle * wc.time_cycle) *  (wc.time_efficiency or 1.0)
-                    ##         price += wc.costs_cycle * cycle + wc.costs_hour * hour
-                    ##         price = self.pool.get('product.uom')._compute_price(cr,uid,bom.product_uom.id,price,bom.product_id.uom_id.id)
-
-            else:
-                res[pr.id] = pr.standard_price
-
+            bom = bom_obj.browse(cursor, user, bom_id)
+            sub_products, routes = bom_obj._bom_explode(cursor, user, bom,
+                                                        factor=1,
+                                                        properties=bom_properties,
+                                                        addthis=True)
+            res[pr.id] = 0.0
+            for sub_product_dict in sub_products:
+                sub_product = self.browse(cursor, user, sub_product_dict['product_id'])
+                std_price = sub_product.standard_price
+                qty = uom_obj._compute_qty(cursor, user,
+                                           from_uom_id = sub_product_dict['product_uom'],
+                                           qty         = sub_product_dict['product_qty'],
+                                           to_uom_id   = sub_product.uom_po_id.id)
+                res[pr.id] += std_price * qty
+                # TODO alf use routes to compute cost of manufacturing (following is from product_extended)
+                ## if bom.routing_id:
+                ##     for wline in bom.routing_id.workcenter_lines:
+                ##         wc = wline.workcenter_id
+                ##         cycle = wline.cycle_nbr
+                ##         hour = (wc.time_start + wc.time_stop + cycle * wc.time_cycle) *  (wc.time_efficiency or 1.0)
+                ##         price += wc.costs_cycle * cycle + wc.costs_hour * hour
+                ##         price = self.pool.get('product.uom')._compute_price(cr,uid,bom.product_uom.id,price,bom.product_id.uom_id.id)
         return res
 
 
-    def _cost_price(self, cr, uid, ids, context=None):
+    def _cost_price(self, cr, uid, ids, field_name, arg, context=None):
         if context is None:
             context = {}
         pricelist = context.get('pricelist')
         product_uom = context.get('product_uom')
         bom_properties = context.get('properties')
-        return  _compute_purchase_price(cr, uid, ids, pricelist, product_uom, bom_properties)
-    
+        res = self._compute_purchase_price(cr, uid, ids, pricelist, product_uom, bom_properties)
+        return res
+
     _columns = {
         'cost_price': fields.function(_cost_price,
                                       methode=True,
