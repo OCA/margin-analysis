09e0b2ff891ca9f84f9f0ce6a2cc3799ae2d2534
Alexandre Fayolle @ camptocamp <alexandre.fayolle@camptocamp.com>
2012-05-16 14:08:11 +0200

    [FIX] product_cost_incl_bom: lift limitations of previous implementation re. BoM structure (lp:c2c-addons/6.1  rev 40.1.24)

--- product_cost_incl_bom/product_cost_incl_bom.py
+++ product_cost_incl_bom/product_cost_incl_bom.py
@@ -26,24 +26,12 @@ import decimal_precision as dp
 class Product(Model):
     _inherit = 'product.product'
 
-    ## def get_cost_field(self):
-    ##     """overriden from product_get_cost_field"""
-    ##     return self.standard_price # XXX or a string?
-
-
-
     def _compute_purchase_price(self, cursor, user, ids,
                                 pricelist=None,
                                 product_uom=None,
                                 bom_properties=None):
         '''
-        Compute the purchase price
-
-        As it explodes the sub product on 1 level
-
-        This is not implemented for BoM having sub BoM producing more than 1
-        product qty.
-        Rewrite _compute_purchase_price and remove mrp constraint to fix this.
+        Compute the purchase price, taking into account sub products and routing
         '''
         if bom_properties is None:
             bom_properties =  []
@@ -58,17 +46,15 @@ class Product(Model):
             # Workaround for first loading in V5 as some columns are not created
             #if not hasattr(pr, 'standard_price'): return False
             bom_id = bom_obj._bom_find(cursor, user, pr.id, product_uom=None, properties=bom_properties)
-
             if not bom_id: # no BoM: use standard_price
                 res[pr.id] = pr.standard_price
                 continue
-
             bom = bom_obj.browse(cursor, user, bom_id)
             sub_products, routes = bom_obj._bom_explode(cursor, user, bom,
                                                         factor=1,
                                                         properties=bom_properties,
                                                         addthis=True)
-            res[pr.id] = 0.0
+            price = 0.
             for sub_product_dict in sub_products:
                 sub_product = self.browse(cursor, user, sub_product_dict['product_id'])
                 std_price = sub_product.standard_price
@@ -76,15 +62,16 @@ class Product(Model):
                                            from_uom_id = sub_product_dict['product_uom'],
                                            qty         = sub_product_dict['product_qty'],
                                            to_uom_id   = sub_product.uom_po_id.id)
-                res[pr.id] += std_price * qty
-                # TODO alf use routes to compute cost of manufacturing (following is from product_extended)
-                ## if bom.routing_id:
-                ##     for wline in bom.routing_id.workcenter_lines:
-                ##         wc = wline.workcenter_id
-                ##         cycle = wline.cycle_nbr
-                ##         hour = (wc.time_start + wc.time_stop + cycle * wc.time_cycle) *  (wc.time_efficiency or 1.0)
-                ##         price += wc.costs_cycle * cycle + wc.costs_hour * hour
-                ##         price = self.pool.get('product.uom')._compute_price(cr,uid,bom.product_uom.id,price,bom.product_id.uom_id.id)
+                price += std_price * qty
+            if bom.routing_id:
+                for wline in bom.routing_id.workcenter_lines:
+                    wc = wline.workcenter_id
+                    cycle = wline.cycle_nbr
+                    hour = (wc.time_start + wc.time_stop + cycle * wc.time_cycle) *  (wc.time_efficiency or 1.0)
+                    price += wc.costs_cycle * cycle + wc.costs_hour * hour
+            price /= bom.product_qty
+            price = uom_obj._compute_price(cursor, user, bom.product_uom.id, price, bom.product_id.uom_id.id)
+            res[pr.id] = price
         return res
 
 
@@ -99,7 +86,7 @@ class Product(Model):
 
     _columns = {
         'cost_price': fields.function(_cost_price,
-                                      methode=True,
+                                      method=True,
                                       string='Cost Price (incl. BoM)',
                                       digits_compute = dp.get_precision('Sale Price'),
                                       help="The cost is the standard price")
