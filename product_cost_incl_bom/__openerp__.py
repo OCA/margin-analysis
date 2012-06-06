105387e6c5917225123e4955208d4ae9e55394d1
Joël Grand-Guillaume <joel.grandguillaume@camptocamp.com>
2012-06-06 15:11:47 +0200

    [MRG] From customer branch (lp:c2c-addons/6.1  rev 28.2.3)

--- product_cost_incl_bom/__openerp__.py
+++ product_cost_incl_bom/__openerp__.py
@@ -22,7 +22,7 @@
  'version' : '0.1',
  'author' : 'Camptocamp',
  'maintainer': 'Camptocamp',
- 'category': 'Hidden',
+ 'category': 'Products',
  'complexity': "normal",  # easy, normal, expert
  'depends' : ['product_get_cost_field', 
               'mrp'],
