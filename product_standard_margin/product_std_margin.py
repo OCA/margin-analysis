8d30020e812b4b48ed0a4d5c7640607e28a4986a
Joël Grand-Guillaume <joel.grandguillaume@camptocamp.com>
2012-07-12 09:45:48 +0200

    [ADD] product_historical_margin_report that override the invoice line analysis to add margin           [IMP] Add tax include/exlude support on margin computation           [FIX] Little Typo and fixes after tests (lp:c2c-addons/6.1  rev 28.6.25)

--- product_standard_margin/product_std_margin.py
+++ product_standard_margin/product_std_margin.py
@@ -29,6 +29,27 @@ import logging
 class Product(Model):
     _inherit = 'product.product'
     
+    #TODO : compute the margin with default taxes
+    def _amount_tax_excluded(self, cr, uid, ids, context=None):
+        """
+        Compute the list price total without tax (in case you are in tax included).
+        This will use the default taxes defined on the product. 
+        :return dict of values:
+            {INT Product ID: 
+                float price without tax
+            }
+        """
+        res = {}
+        if context is None:
+            context = {}
+        tax_obj = self.pool.get('account.tax')
+        cur_obj = self.pool.get('res.currency')
+        for prod in self.browse(cr, uid, ids):
+            price = prod.list_price
+            taxes = tax_obj.compute_all(cr, uid, prod.taxes_id, price, 1, product=prod.id)
+            res[prod.id] = taxes['total']
+        return res
+        
     def _compute_margin(self, cursor, user, ids, field_name, arg, context = None):
         """
         Calculate the margin based on product infos. Take care of the cost_field 
@@ -38,8 +59,8 @@ class Product(Model):
         We don't take care of the product price type currency to remove the dependency on
         the sale module. We consider the cost and sale price is in the company currency.
         
-        We don't take care of the taxes, and base our computation on the hypothesis that 
-        all prices are VAT excluded.
+        We take care of the default product taxes, and base our computation on total without 
+        tax.
         
         :return dict of dict of the form : 
             {INT Product ID : {
@@ -57,7 +78,8 @@ class Product(Model):
             res[product] = {'margin_absolute': 0, 'margin_relative': 0}
         for product in self.browse(cursor, user, ids):
             cost = product.cost_price
-            sale = product.list_price
+            sale = self._amount_tax_excluded(cursor, user, [product.id], context=context)[product.id]
+            # sale = product.list_price
             res[product.id]['standard_margin'] = sale - cost
             if sale == 0:
                 logger.debug("Sale price for product ID %d is 0, cannot compute margin rate...", product.id)
@@ -72,13 +94,13 @@ class Product(Model):
                                               string='Theorical Margin',
                                               digits_compute=dp.get_precision('Sale Price'),
                                               multi ='margin',
-                                              help='Theorical Margin is [ sale price - cost price ] of the product form (not based on historical values).'
-                                              'Prices should be VAT excluded. If no sale price, the margin will be negativ.'),
+                                              help='Theorical Margin is [ sale price (Wo Tax) - cost price ] of the product form (not based on historical values).'
+                                              'Take care of tax include and exclude. If no sale price, the margin will be negativ.'),
         'standard_margin_rate' : fields.function(_compute_margin,
                                         method=True,
                                         string='Theorical Margin (%)',
                                         digits_compute=dp.get_precision('Sale Price'),
                                         multi='margin',
-                                        help='Markup rate is [ Theorical Margin / sale price ] of the product form (not based on historical values).'
-                                        'Prices should be VAT excluded. If no sale price set, will display 999.0'),
+                                        help='Markup rate is [ Theorical Margin / sale price (Wo Tax) ] of the product form (not based on historical values).'
+                                        'Take care of tax include and exclude.. If no sale price set, will display 999.0'),
         }
