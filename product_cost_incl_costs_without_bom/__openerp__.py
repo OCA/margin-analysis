df69d60c167918d023cb709dfef82cd0083ce89d
erwin-endiansolutions <erwin@endiansolutions.nl>
2012-12-30 13:22:27 +0100

    Two extra modules for adding fixed costs to a product. One module is depending on the Bom, the other module isn't.

---
+++ product_cost_incl_costs_without_bom/__openerp__.py
@@ -0,0 +1,38 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+#
+#    Copyright 2012 Camptocamp SA
+#    Copyright 2012 Endian Solutions BV
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################{'name' : 'Product Cost field',
+ 'version' : '0.1',
+ 'author' : 'Grishma Shukla',
+ 'maintainer': 'Endian Solutions',
+ 'category': 'Products',
+ 'complexity': "normal",  
+ 'depends' : ['product_get_cost_field',
+              ],
+ 'description': """
+ This module adds an Fixed cost field to the product form. So you can calculate a costprice with added costs, without the use of a BoM.
+ """,
+ 'website': 'www.endiansolutions.nl',
+ 'init_xml': [],
+ 'update_xml': ['product_view.xml'],
+ 'demo_xml': [],
+ 'tests': [],
+ 'installable': True,
+ 'application': False
+ }
