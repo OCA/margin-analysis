2b7764f651d2fa2895380fe32efefbbfea7d7864
Yannick Vaucher <yannick.vaucher@camptocamp.com>
2013-10-14 20:16:46 +0200

    [PEP8]

--- product_standard_margin/__openerp__.py
+++ product_standard_margin/__openerp__.py
@@ -32,17 +32,16 @@
 
   It will just compute it as follow: (Sale Price without tax - Cost Price) / Sale Price without tax
 
-  Remember that this module can be used in conjonction with product_cost_incl_bom to have the 
+  Remember that this module can be used in conjonction with product_cost_incl_bom to have the
   cost price computed from the BOM when a product has one.
 
   WARNING:
 
   1) As this module will base his simple computation on sale and cost prices, it suppose
-  you have them both in the same currency (the price type must of the same currency for both of 
-  them). Remember this is the default OpenERP configuration (price type of all product price 
+  you have them both in the same currency (the price type must of the same currency for both of
+  them). Remember this is the default OpenERP configuration (price type of all product price
   fields are set as the same as the company currency). We don't take care of it cause otherwise
   we should have added a dependency on sale module.
-
 
   """,
  'website': 'http://www.camptocamp.com/',
@@ -54,5 +53,3 @@
  'auto_install': False,
  'license': 'AGPL-3',
  'application': True}
-
-
