45a09a6abee826bb39e796d9486512de8531122a
Joel Grand-Guillaume <joel.grandguillaume@camptocamp.com>
2013-11-25 14:52:59 +0100

    [MRG] From trunk (../port-add-product_multi_company_7.0-bis-jge/ rev 230)

--- product_price_history/__openerp__.py
+++ product_price_history/__openerp__.py
@@ -25,31 +25,36 @@
     "version" : "1.2",
     "author" : "Camptocamp",
     "category" : "Generic Modules/Inventory Control",
-    "depends" : [ "product","purchase"],
+    "depends" : ["product",
+                 "purchase",
+                 ],
     "description": """
 Product Price History
 =====================
 
-This module allow you to :
+This module allows you to:
 
-* Record various prices of a same product for different companies. This way, every company can have his own cost (average or standard) and sale price. 
-* Historize the prices in a way that you'll then be able to retrieve the cost (or sale) price at a given date.
+* Record various prices of a same product for different companies. This
+  way, every company can have its own costs (average or standard) and
+  sale prices.
+* Historize the prices in a way that you'll then be able to retrieve the
+  cost (or sale) price at a given date.
 
 Note that to benefit those values in stock report (or any other view that is based on SQL),
 you'll have to adapt it to include this new historized table. Especially true for stock
 valuation.
 
-This module also contain demo data and various tests to ensure it work well. It show 
+This module also contains demo data and various tests to ensure it works well. It shows
 how to configure OpenERP properly when you have various company, each of them having 
-their product setup in average price and using different currency. The goal is to share
-the products between all company, keeping the right price for each of them.
+their product setup in average price and using different currencies. The goal is to share
+the products between all companies, keeping the right price for each of them.
 
 Technically, this module updates the definition of field standard_price, list_price 
 of the product and will make them stored in an external table. We override the read, 
 write and create methods to achieve that and don't used ir.property for performance
-and historization purpose. 
+and historization purpose.
 
-You may want to also use the module analytic_multicurrency from  bzr branch lp:account-analytic
+You may want to also use the module analytic_multicurrency from `bzr branch lp:account-analytic/7.0`
 in order to have a proper computation in analytic line as well (standard_price will be converted
 in company currency with this module when computing cost of analytic line).
 """,
--- product_price_history/wizard/historic_prices.py
+++ product_price_history/wizard/historic_prices.py
@@ -28,7 +28,8 @@ class historic_prices(orm.TransientModel):
     _description = 'Product historical prices'
 
     _columns = {
-        'to_date': fields.date('Date', 
+        'to_date': fields.date(
+            'Date',
             help='Date at which the analysis need to be done. '
             'Note that the date is understood as this day at midnight, so you may want to '
             'specify the day after ! No date is the last value.'),
@@ -38,7 +39,6 @@ class historic_prices(orm.TransientModel):
         """
         Open the historical prices view
         """
-
         if context is None:
             context = {}
         user_obj = self.pool.get('res.users')
