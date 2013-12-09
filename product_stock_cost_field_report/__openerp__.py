6205ca9ef6b1efa64b3e6fbfc9f812003203621d
Joel Grand-Guillaume <joel.grandguillaume@camptocamp.com>
2013-12-09 12:06:38 +0100

    [ADD] product_stock_report_field_report to change OpenERP SQL view to use the cost_price field

---
+++ product_stock_cost_field_report/__openerp__.py
@@ -0,0 +1,44 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+#
+#    Author:  Joel Grand-Guillaume
+#    Copyright 2013 Camptocamp SA
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+{'name' : 'Product Stock Cost field',
+ 'version' : '1.0',
+ 'author' : 'Camptocamp',
+ 'maintainer': 'Camptocamp',
+ 'category': 'Products',
+ 'complexity': "normal",  # easy, normal, expert
+ 'depends' : [
+        'product_get_cost_field',
+        'stock',
+              ],
+ 'description': """
+This module override the reporting view of OpenERP to replace the standard_price field used
+by the new cost_price one. This way all reporting of OpenERP will now take this field into 
+account and display the correct result.
+ """,
+ 'website': 'http://www.camptocamp.com/',
+ 'data': [],
+ 'demo': [],
+ 'test': [],
+ 'installable': True,
+ 'auto_install': True,
+ 'license': 'AGPL-3',
+ 'application': False
+ }
