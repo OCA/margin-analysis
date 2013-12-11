8431205ec8a862f5d0954adf53993bfb8b4a97d6
Yannick Vaucher <yannick.vaucher@camptocamp.com>
2013-12-11 16:30:40 +0100

    [PEP8] product_standard_margin

--- product_standard_margin/product_std_margin.py
+++ product_standard_margin/product_std_margin.py
@@ -26,18 +26,21 @@ import decimal_precision as dp
 import logging
 _logger = logging.getLogger(__name__)
 
+
 class Product(orm.Model):
     _inherit = 'product.product'
 
     #TODO : compute the margin with default taxes
     def _amount_tax_excluded(self, cr, uid, ids, context=None):
-        """
-        Compute the list price total without tax (in case you are in tax included).
+        """ Compute the list price total without tax
+
+        (in case you are in tax included).
         This will use the default taxes defined on the product.
         :return dict of values:
             {INT Product ID:
                 float price without tax
             }
+
         """
         res = {}
         if context is None:
@@ -49,25 +52,27 @@ class Product(orm.Model):
             res[prod.id] = taxes['total']
         return res
 
-    def _compute_margin(self, cursor, user, ids, field_name, arg, context=None):
-        """
-        Calculate the margin based on product infos. Take care of the cost_field
-        define in product_get_cost_field. So the margin will be computed based on this
-        field.
+    def _compute_margin(self, cr, user, ids, field_name, arg, context=None):
+        """ Calculate the margin based on product infos.
 
-        We don't take care of the product price type currency to remove the dependency on
-        the sale module. We consider the cost and sale price is in the company currency.
+        Take care of the cost_field define in product_get_cost_field. So the
+        margin will be computed based on this field.
 
-        We take care of the default product taxes, and base our computation on total without
-        tax.
+        We don't take care of the product price type currency to remove the
+        dependency on the sale module. We consider the cost and sale price is
+        in the company currency.
+
+        We take care of the default product taxes, and base our computation on
+        total without tax.
 
         :return dict of dict of the form :
             {INT Product ID : {
                 {'margin_absolute': float,
                  'margin_relative': float}
             }}
+
         """
-        
+
         if context is None:
             context = {}
         res = {}
@@ -75,13 +80,16 @@ class Product(orm.Model):
             return res
         for product in ids:
             res[product] = {'margin_absolute': 0, 'margin_relative': 0}
-        for product in self.read(cursor, user, ids, ['id','cost_price'], context=context):
+        for product in self.read(cr, user, ids,
+                                 ['id', 'cost_price'], context=context):
             cost = product['cost_price']
-            sale = self._amount_tax_excluded(cursor, user,
-                    [product['id']], context=context)[product['id']]
+            sale = self._amount_tax_excluded(cr, user,
+                                             [product['id']],
+                                             context=context)[product['id']]
             res[product['id']]['standard_margin'] = sale - cost
             if sale == 0:
-                _logger.debug("Sale price for product ID %d is 0, cannot compute margin rate...", product['id'])
+                _logger.debug("Sale price for product ID %d is 0, cannot "
+                              "compute margin rate...", product['id'])
                 res[product['id']]['standard_margin_rate'] = 999.
             else:
                 res[product['id']]['standard_margin_rate'] = (sale - cost) / sale * 100
@@ -89,23 +97,23 @@ class Product(orm.Model):
 
     _columns = {
         'standard_margin': fields.function(
-                _compute_margin,
-                method=True,
-                string='Theorical Margin',
-                digits_compute=dp.get_precision('Sale Price'),
-                multi='margin',
-                help='Theorical Margin is [ sale price (Wo Tax) - cost price ] '
-                    'of the product form (not based on historical values). '
-                    'Take care of tax include and exclude. If no sale price, '
-                    'the margin will be negativ.'),
+            _compute_margin,
+            method=True,
+            string='Theorical Margin',
+            digits_compute=dp.get_precision('Sale Price'),
+            multi='margin',
+            help='Theorical Margin is [ sale price (Wo Tax) - cost price ] '
+                 'of the product form (not based on historical values). '
+                 'Take care of tax include and exclude. If no sale price, '
+                 'the margin will be negativ.'),
         'standard_margin_rate': fields.function(
-                _compute_margin,
-                method=True,
-                string='Theorical Margin (%)',
-                digits_compute=dp.get_precision('Sale Price'),
-                multi='margin',
-                help='Markup rate is [ Theorical Margin / sale price (Wo Tax) ] '
-                    'of the product form (not based on historical values).'
-                    'Take care of tax include and exclude.. If no sale price '
-                    'set, will display 999.0'),
-        }
+            _compute_margin,
+            method=True,
+            string='Theorical Margin (%)',
+            digits_compute=dp.get_precision('Sale Price'),
+            multi='margin',
+            help='Markup rate is [ Theorical Margin / sale price (Wo Tax) ] '
+                 'of the product form (not based on historical values).'
+                 'Take care of tax include and exclude.. If no sale price '
+                 'set, will display 999.0'),
+    }
