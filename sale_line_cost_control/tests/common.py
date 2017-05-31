349075f9689ddaa25def418e6b81bfec5bf09da1
Guewen Baconnier <guewen.baconnier@camptocamp.com>
2017-05-31 09:34:35 +0200

    Add name on group for easier customization

--- sale_line_cost_control/tests/common.py
+++ sale_line_cost_control/tests/common.py
@@ -1,9 +1,6 @@
 # -*- coding: utf-8 -*-
 # Copyright 2017 Camptocamp SA
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html)
-# -*- coding: utf-8 -*-
-# Copyright 2017 Camptocamp SA
-# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html)
 
 
 from odoo.addons.sale.tests import test_sale_common
