3ae96485dd4b459219fb44b6da1e2829b828ced9
David <david.vidal@tecnativa.com>
2018-07-09 18:59:44 +0200

    [ADD] sale_margin_delivered: New Module

---
+++ sale_margin_delivered/__manifest__.py
@@ -0,0 +1,19 @@
+# Copyright 2018 Tecnativa - Sergio Teruel
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+{
+    "name": "Sale Margin Delivered",
+    "version": "11.0.1.0.0",
+    "author": "Tecnativa,"
+              "Odoo Community Association (OCA)",
+    "website": "https://github.com/OCA/margin-analysis",
+    "category": "Sales",
+    "license": "AGPL-3",
+    "depends": [
+        "stock",
+        "sale_margin_security",
+    ],
+    "data": [
+        "views/sale_margin_delivered_view.xml",
+    ],
+    "installable": True,
+}
