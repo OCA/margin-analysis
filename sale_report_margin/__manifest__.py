4081627509d97bc7485ecec432943303f5fb68b1
David Vidal <david.vidal@tecnativa.com>
2018-09-21 12:14:17 +0200

    [ADD] sale_report_margin: New Module (#38)

---
+++ sale_report_margin/__manifest__.py
@@ -0,0 +1,15 @@
+# Copyright 2018 Tecnativa - Sergio Teruel
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+{
+    "name": "Sale Report Margin",
+    "version": "11.0.1.0.0",
+    'author': 'Tecnativa,'
+              'Odoo Community Association (OCA)',
+    'website': 'https://github.com/OCA/sale-reporting',
+    "category": "Sales",
+    "license": "AGPL-3",
+    "depends": [
+        "sale_margin",
+    ],
+    "installable": True,
+}
