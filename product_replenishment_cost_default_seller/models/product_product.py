833faee33b3576823d8e0265625e91a5528738fc
Marc Poch @PlanetaTIC <mpoch@planetatic.com>
2019-09-23 11:46:59 +0200

    [FIX] product_replenishment_cost_default_seller: Fixed flake8 errors

--- product_replenishment_cost_default_seller/models/product_product.py
+++ product_replenishment_cost_default_seller/models/product_product.py
@@ -2,10 +2,8 @@
 # Copyright 2019 PlanetaTIC - Marc Poch <mpoch@planetatic.com>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
-from odoo import fields, api
+from odoo import api
 from odoo.models import Model
-
-import odoo.addons.decimal_precision as dp
 
 
 class ProductProduct(Model):
