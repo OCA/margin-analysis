f99ec086b237e74fff7041140357aa0d6695bee7
Marc Poch @PlanetaTIC <mpoch@planetatic.com>
2019-09-23 11:51:09 +0200

    [FIX] product_replenishment_cost_supplier_discount: fixed flake8 errors.

--- product_replenishment_cost_supplier_discount/models/product.py
+++ product_replenishment_cost_supplier_discount/models/product.py
@@ -2,9 +2,7 @@
 # Copyright 2019 PlanetaTIC - Marc Poch <mpoch@planetatic.com>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
-from odoo import models, fields, api, _
-
-import odoo.addons.decimal_precision as dp
+from odoo import models, api
 
 
 class ProductProduct(models.Model):
