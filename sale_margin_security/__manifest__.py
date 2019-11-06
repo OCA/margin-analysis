2747385c399013048565a0e782db817386f7744d
Sergio Teruel <sergio.teruel@tecnativa.com>
2019-11-06 21:21:19 +0100

    [MIG] sale_margin_security: Migration to v12.0

--- sale_margin_security/__manifest__.py
+++ sale_margin_security/__manifest__.py
@@ -2,12 +2,14 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 {
     "name": "Sale Margin Security",
-    "version": "11.0.1.0.0",
+    "version": "12.0.1.0.0",
     "author": 'Tecnativa,'
               'Odoo Community Association (OCA)',
     "website": 'https://github.com/OCA/margin-analysis',
     "category": "Sales",
     "license": "AGPL-3",
+    "development_status": "Production/Stable",
+    "maintainers": ["sergio-teruel"],
     "depends": [
         "sale_margin",
     ],
