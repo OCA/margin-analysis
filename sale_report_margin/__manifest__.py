75d294f32c88588caf9f2a59f5c95fbc5ed80888
Sergio Teruel <sergio.teruel@tecnativa.com>
2019-11-29 15:49:08 +0100

    [MIG] sale_report_margin: Migration to v12.0

--- sale_report_margin/__manifest__.py
+++ sale_report_margin/__manifest__.py
@@ -2,10 +2,12 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 {
     "name": "Sale Report Margin",
-    "version": "11.0.1.0.0",
-    'author': 'Tecnativa,'
-              'Odoo Community Association (OCA)',
-    'website': 'https://github.com/OCA/sale-reporting',
+    "version": "12.0.1.0.0",
+    "author": "Tecnativa,"
+              "Odoo Community Association (OCA)",
+    "website": "https://github.com/OCA/sale-reporting",
+    "development_status": "Production/Stable",
+    "maintainers": ["sergio-teruel"],
     "category": "Sales",
     "license": "AGPL-3",
     "depends": [
