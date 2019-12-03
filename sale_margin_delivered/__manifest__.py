105bd1e2bbcc2ca8b28edd0d14f244f1459a0a62
Sergio Teruel <sergio.teruel@tecnativa.com>
2019-12-03 15:29:00 +0100

    [IMP] stock_picking_comment_template: Change field label to fix travis warning

--- sale_margin_delivered/__manifest__.py
+++ sale_margin_delivered/__manifest__.py
@@ -2,7 +2,7 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 {
     "name": "Sale Margin Delivered",
-    "version": "11.0.1.0.0",
+    "version": "12.0.1.0.0",
     "author": "Tecnativa,"
               "Odoo Community Association (OCA)",
     "website": "https://github.com/OCA/margin-analysis",
@@ -16,4 +16,6 @@
         "views/sale_margin_delivered_view.xml",
     ],
     "installable": True,
+    "development_status": "Production/Stable",
+    "maintainers": ["sergio-teruel"],
 }
