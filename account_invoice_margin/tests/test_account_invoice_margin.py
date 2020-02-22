c4c2b3302690f34abd5212222cdd710d2529b963
Sergio Teruel <sergio.teruel@tecnativa.com>
2020-02-22 21:23:31 +0100

    [MIG] account_invoice_margin: Migration to v13.0

--- account_invoice_margin/tests/test_account_invoice_margin.py
+++ account_invoice_margin/tests/test_account_invoice_margin.py
@@ -1,6 +1,7 @@
 # © 2016 Sergio Teruel <sergio.teruel@tecnativa.com>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
+from odoo import fields
 from odoo.tests.common import SavepointCase, tagged
 
 
@@ -15,7 +16,7 @@ class TestAccountInvoiceMargin(SavepointCase):
             {"name": "Test journal", "type": "sale", "code": "TEST_J"}
         )
         cls.account_type = cls.env["account.account.type"].create(
-            {"name": "Test account type", "type": "receivable"}
+            {"name": "Test account type", "type": "other", "internal_group": "income"}
         )
         cls.account = cls.env["account.account"].create(
             {
@@ -26,7 +27,7 @@ class TestAccountInvoiceMargin(SavepointCase):
             }
         )
         cls.partner = cls.env["res.partner"].create(
-            {"name": "Test partner", "customer": True, "is_company": True}
+            {"name": "Test partner", "customer_rank": 1, "is_company": True}
         )
         cls.partner.property_account_receivable_id = cls.account
         cls.product_categ = cls.env["product.category"].create(
@@ -45,19 +46,19 @@ class TestAccountInvoiceMargin(SavepointCase):
             }
         )
         cls.product.property_account_receivable_id = cls.account
-        cls.invoice = cls.env["account.invoice"].create(
+        cls.invoice = cls.env["account.move"].create(
             {
                 "partner_id": cls.partner.id,
-                "date_invoice": "2017-06-19",
+                "invoice_date": fields.Date.from_string("2017-06-19"),
                 "type": "out_invoice",
                 "currency_id": cls.env.user.company_id.currency_id.id,
-                "account_id": cls.partner.property_account_receivable_id.id,
                 "invoice_line_ids": [
                     (
                         0,
-                        0,
+                        None,
                         {
                             "product_id": cls.product.id,
+                            "product_uom_id": cls.product.uom_id.id,
                             "account_id": cls.product.property_account_receivable_id.id,
                             "name": "Test Margin",
                             "price_unit": cls.product.list_price,
@@ -73,17 +74,29 @@ class TestAccountInvoiceMargin(SavepointCase):
         self.assertEqual(self.invoice.invoice_line_ids.purchase_price, 100.00)
         self.assertEqual(self.invoice.invoice_line_ids.margin, 1000.00)
 
-        self.invoice.invoice_line_ids.discount = 50
+        self.invoice.invoice_line_ids.with_context(
+            check_move_validity=False
+        ).discount = 50
         self.assertEqual(self.invoice.invoice_line_ids.margin, 0.0)
 
     def test_invoice_margin_uom(self):
         inv_line = self.invoice.invoice_line_ids
-        inv_line.write({"uom_id": self.env.ref("uom.product_uom_dozen").id})
-        inv_line._onchange_uom_id()
-        inv_line._onchange_product_id_account_invoice_margin()
+        inv_line.update({"product_uom_id": self.env.ref("uom.product_uom_dozen").id})
+        inv_line.with_context(check_move_validity=False)._onchange_uom_id()
         self.assertEqual(inv_line.margin, 12000.00)
 
     def test_invoice_refund(self):
-        new_invoice = self.invoice.refund()
+        self.invoice.action_post()
+        wiz = (
+            self.env["account.move.reversal"]
+            .with_context(
+                active_model="account.move",
+                active_ids=self.invoice.ids,
+                active_id=self.invoice.id,
+            )
+            .create({"refund_method": "refund", "reason": "reason test create"})
+        )
+        action = wiz.reverse_moves()
+        new_invoice = self.env["account.move"].browse(action["res_id"])
         self.assertEqual(new_invoice.invoice_line_ids.margin, 1000.00)
         self.assertEqual(new_invoice.invoice_line_ids.margin_signed, -1000.00)
