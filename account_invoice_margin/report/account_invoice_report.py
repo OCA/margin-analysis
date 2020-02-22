9c2fcdbb8830fef9572b3db97b58a76966f4c77d
Sergio Teruel <sergio.teruel@tecnativa.com>
2020-02-22 21:23:31 +0100

    [IMP] account_invoice_margin: black, isort

--- account_invoice_margin/report/account_invoice_report.py
+++ account_invoice_margin/report/account_invoice_report.py
@@ -7,12 +7,12 @@ from odoo import fields, models
 class AccountInvoiceReport(models.Model):
     _inherit = "account.invoice.report"
 
-    margin = fields.Float(string='Margin', readonly=True)
+    margin = fields.Float(string="Margin", readonly=True)
 
     def _select(self):
         select_str = super()._select()
-        return '%s, sub.margin' % select_str
+        return "%s, sub.margin" % select_str
 
     def _sub_select(self):
         select_str = super()._sub_select()
-        return '%s, SUM(ail.margin_signed) AS margin' % select_str
+        return "%s, SUM(ail.margin_signed) AS margin" % select_str
