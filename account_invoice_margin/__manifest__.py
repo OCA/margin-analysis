4378ece524fa74a11a43c5de6fa87b498a53a96f
Sergio Teruel <sergio.teruel@tecnativa.com>
2020-02-22 21:23:31 +0100

    [MIG] account_invoice_margin: Migration to v13.0

--- account_invoice_margin/__manifest__.py
+++ account_invoice_margin/__manifest__.py
@@ -4,7 +4,7 @@
 {
     "name": "Account Invoice Margin",
     "summary": "Show margin in invoices",
-    "version": "12.0.2.0.0",
+    "version": "13.0.1.0.0",
     "category": "Account",
     "website": "https://www.github.com/OCA/margin-analysis",
     "author": "Tecnativa, " "GRAP, " "Odoo Community Association (OCA)",
@@ -17,6 +17,5 @@
     "data": [
         "security/account_invoice_margin_security.xml",
         "views/account_invoice_margin_view.xml",
-        "data/res_groups.xml",
     ],
 }
