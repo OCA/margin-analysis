6bb98ed0e433fffe27c001d7d48e691ce05c41b3
Sergio Teruel <sergio.teruel@tecnativa.com>
2020-02-22 21:23:31 +0100

    [IMP] account_invoice_margin: black, isort

--- account_invoice_margin/tests/test_account_invoice_margin.py
+++ account_invoice_margin/tests/test_account_invoice_margin.py
@@ -4,66 +4,70 @@
 from odoo.tests.common import SavepointCase, tagged
 
 
-@tagged('post_install', '-at_install')
+@tagged("post_install", "-at_install")
 class TestAccountInvoiceMargin(SavepointCase):
-
     @classmethod
     def setUpClass(cls):
         super(TestAccountInvoiceMargin, cls).setUpClass()
-        cls.Product = cls.env['product.template']
+        cls.Product = cls.env["product.template"]
 
-        cls.journal = cls.env["account.journal"].create({
-            "name": "Test journal",
-            "type": "sale",
-            "code": "TEST_J",
-        })
-        cls.account_type = cls.env["account.account.type"].create({
-            "name": "Test account type",
-            "type": "receivable",
-        })
-        cls.account = cls.env["account.account"].create({
-            "name": "Test account",
-            "code": "TEST_A",
-            "user_type_id": cls.account_type.id,
-            "reconcile": True,
-        })
-        cls.partner = cls.env["res.partner"].create({
-            "name": "Test partner",
-            "customer": True,
-            "is_company": True,
-        })
+        cls.journal = cls.env["account.journal"].create(
+            {"name": "Test journal", "type": "sale", "code": "TEST_J"}
+        )
+        cls.account_type = cls.env["account.account.type"].create(
+            {"name": "Test account type", "type": "receivable"}
+        )
+        cls.account = cls.env["account.account"].create(
+            {
+                "name": "Test account",
+                "code": "TEST_A",
+                "user_type_id": cls.account_type.id,
+                "reconcile": True,
+            }
+        )
+        cls.partner = cls.env["res.partner"].create(
+            {"name": "Test partner", "customer": True, "is_company": True}
+        )
         cls.partner.property_account_receivable_id = cls.account
-        cls.product_categ = cls.env["product.category"].create({
-            "name": "Test category"
-        })
+        cls.product_categ = cls.env["product.category"].create(
+            {"name": "Test category"}
+        )
 
-        cls.product = cls.env["product.product"].create({
-            "name": "test product",
-            "categ_id": cls.product_categ.id,
-            "uom_id": cls.env.ref('uom.product_uom_unit').id,
-            "uom_po_id": cls.env.ref('uom.product_uom_unit').id,
-            "default_code": "test-margin",
-            "list_price": 200.00,
-            "standard_price": 100.00,
-        })
+        cls.product = cls.env["product.product"].create(
+            {
+                "name": "test product",
+                "categ_id": cls.product_categ.id,
+                "uom_id": cls.env.ref("uom.product_uom_unit").id,
+                "uom_po_id": cls.env.ref("uom.product_uom_unit").id,
+                "default_code": "test-margin",
+                "list_price": 200.00,
+                "standard_price": 100.00,
+            }
+        )
         cls.product.property_account_receivable_id = cls.account
-        cls.invoice = cls.env['account.invoice'].create({
-            'partner_id': cls.partner.id,
-            'date_invoice': '2017-06-19',
-            'type': 'out_invoice',
-            'currency_id': cls.env.user.company_id.currency_id.id,
-            'account_id': cls.partner.property_account_receivable_id.id,
-            'invoice_line_ids': [
-                (0, 0, {
-                    'product_id': cls.product.id,
-                    'account_id':
-                        cls.product.property_account_receivable_id.id,
-                    'name': 'Test Margin',
-                    'price_unit': cls.product.list_price,
-                    'quantity': 10,
-                    'purchase_price': cls.product.standard_price,
-                })],
-        })
+        cls.invoice = cls.env["account.invoice"].create(
+            {
+                "partner_id": cls.partner.id,
+                "date_invoice": "2017-06-19",
+                "type": "out_invoice",
+                "currency_id": cls.env.user.company_id.currency_id.id,
+                "account_id": cls.partner.property_account_receivable_id.id,
+                "invoice_line_ids": [
+                    (
+                        0,
+                        0,
+                        {
+                            "product_id": cls.product.id,
+                            "account_id": cls.product.property_account_receivable_id.id,
+                            "name": "Test Margin",
+                            "price_unit": cls.product.list_price,
+                            "quantity": 10,
+                            "purchase_price": cls.product.standard_price,
+                        },
+                    )
+                ],
+            }
+        )
 
     def test_invoice_margin(self):
         self.assertEqual(self.invoice.invoice_line_ids.purchase_price, 100.00)
@@ -74,9 +78,7 @@ class TestAccountInvoiceMargin(SavepointCase):
 
     def test_invoice_margin_uom(self):
         inv_line = self.invoice.invoice_line_ids
-        inv_line.write({
-            'uom_id': self.env.ref('uom.product_uom_dozen').id,
-        })
+        inv_line.write({"uom_id": self.env.ref("uom.product_uom_dozen").id})
         inv_line._onchange_uom_id()
         inv_line._onchange_product_id_account_invoice_margin()
         self.assertEqual(inv_line.margin, 12000.00)
