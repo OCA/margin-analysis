5b1d7a7d37839020a927e98e8d2156acd251126a
Sergio Teruel <sergio.teruel@tecnativa.com>
2020-02-24 11:25:43 +0100

    [IMP] sale_margin_delivered: black, isort

--- sale_margin_delivered/__manifest__.py
+++ sale_margin_delivered/__manifest__.py
@@ -3,18 +3,12 @@
 {
     "name": "Sale Margin Delivered",
     "version": "12.0.1.0.0",
-    "author": "Tecnativa,"
-              "Odoo Community Association (OCA)",
+    "author": "Tecnativa," "Odoo Community Association (OCA)",
     "website": "https://github.com/OCA/margin-analysis",
     "category": "Sales",
     "license": "AGPL-3",
-    "depends": [
-        "stock",
-        "sale_margin_security",
-    ],
-    "data": [
-        "views/sale_margin_delivered_view.xml",
-    ],
+    "depends": ["stock", "sale_margin_security"],
+    "data": ["views/sale_margin_delivered_view.xml"],
     "installable": True,
     "development_status": "Production/Stable",
     "maintainers": ["sergio-teruel"],
