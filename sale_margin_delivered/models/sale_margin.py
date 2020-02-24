fa9897db6addf452da16e29177b0dcb0eb2dca65
Sergio Teruel <sergio.teruel@tecnativa.com>
2020-02-24 11:25:43 +0100

    [IMP] sale_margin_delivered: black, isort

--- sale_margin_delivered/models/sale_margin.py
+++ sale_margin_delivered/models/sale_margin.py
@@ -1,36 +1,32 @@
 # Copyright 2018 Tecnativa - Sergio Teruel
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
-from odoo import api, fields, models
-from odoo import tools
+from odoo import api, fields, models, tools
 
 
 class SaleOrderLine(models.Model):
     _inherit = "sale.order.line"
 
     margin_delivered = fields.Float(
-        string='Margin Delivered',
-        compute='_compute_margin_delivered',
-        store=True,
+        string="Margin Delivered", compute="_compute_margin_delivered", store=True
     )
     margin_delivered_percent = fields.Float(
-        string='Margin Delivered Percent',
-        compute='_compute_margin_delivered',
+        string="Margin Delivered Percent",
+        compute="_compute_margin_delivered",
         store=True,
         readonly=True,
-        oldname='margin_delivered_percent',
+        oldname="margin_delivered_percent",
     )
     purchase_price_delivery = fields.Float(
-        string='Purchase Price Delivered',
-        compute='_compute_margin_delivered',
+        string="Purchase Price Delivered",
+        compute="_compute_margin_delivered",
         store=True,
     )
 
-    @api.depends('margin', 'qty_delivered', 'product_uom_qty',
-                 'move_ids.price_unit')
+    @api.depends("margin", "qty_delivered", "product_uom_qty", "move_ids.price_unit")
     def _compute_margin_delivered(self):
-        digits = self.env['decimal.precision'].precision_get('Product Price')
-        for line in self.filtered('price_reduce'):
+        digits = self.env["decimal.precision"].precision_get("Product Price")
+        for line in self.filtered("price_reduce"):
             if not line.qty_delivered and not line.product_uom_qty:
                 continue
             qty = line.qty_delivered or line.product_uom_qty
@@ -40,21 +36,32 @@ class SaleOrderLine(models.Model):
                 cost_price = 0.0
                 moves = line.move_ids.filtered(
                     lambda x: (
-                        x.state == 'done' and (
-                            x.picking_code == 'outgoing' or (
-                                x.picking_code == 'incoming' and x.to_refund))
-                    ))
+                        x.state == "done"
+                        and (
+                            x.picking_code == "outgoing"
+                            or (x.picking_code == "incoming" and x.to_refund)
+                        )
+                    )
+                )
                 for move in moves:
                     cost_price += move.product_qty * move.price_unit
-                average_price = (abs(cost_price) /
-                                 line.qty_delivered) or line.purchase_price
+                average_price = (
+                    abs(cost_price) / line.qty_delivered
+                ) or line.purchase_price
                 line.purchase_price_delivery = tools.float_round(
-                    average_price, precision_digits=digits)
+                    average_price, precision_digits=digits
+                )
                 line.margin_delivered = line.qty_delivered * (
                     line.price_reduce - line.purchase_price_delivery
                 )
             # compute percent margin based on delivered quantities or ordered
             # quantities
-            line.margin_delivered_percent = qty and (
-                (line.price_reduce - line.purchase_price_delivery) /
-                line.price_reduce * 100.0) or 0.0
+            line.margin_delivered_percent = (
+                qty
+                and (
+                    (line.price_reduce - line.purchase_price_delivery)
+                    / line.price_reduce
+                    * 100.0
+                )
+                or 0.0
+            )
