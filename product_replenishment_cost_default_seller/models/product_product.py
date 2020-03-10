d8a717206213af432359e59467cd929835254ce8
Francisco Fernández @PlanetaTIC <ffernandez@planetatic.com>
2020-03-10 11:07:49 +0100

    [FIX] product_replenishment_cost_default_seller: do not depend on standard_price

--- product_replenishment_cost_default_seller/models/product_product.py
+++ product_replenishment_cost_default_seller/models/product_product.py
@@ -2,19 +2,21 @@
 # Copyright 2019 PlanetaTIC - Marc Poch <mpoch@planetatic.com>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
-from odoo import api
+from odoo import api, fields
 from odoo.models import Model
 
 
 class ProductProduct(Model):
     _inherit = 'product.product'
 
+    replenishment_cost = fields.Float(
+        compute='_get_replenishment_cost_default_seller')
+
     @api.multi
-    @api.depends('product_tmpl_id.standard_price', 'standard_price',
-                 'product_tmpl_id.seller_ids.price')
-    def _get_replenishment_cost(self):
+    @api.depends('product_tmpl_id.seller_ids.price')
+    def _get_replenishment_cost_default_seller(self):
         for product in self:
             if product.seller_ids:
                 product.replenishment_cost = product.seller_ids[0].price
             else:
-                super(ProductProduct, product)._get_replenishment_cost()
+                product.replenishment_cost = 0.0
