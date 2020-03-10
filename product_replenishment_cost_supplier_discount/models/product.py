c84ad4fb6e4123cd8516414d490db5ee00bf190d
Francisco Fernández @PlanetaTIC <ffernandez@planetatic.com>
2020-03-10 11:12:10 +0100

    [FIX] product_replenishment_cost_supplier_discount: do not depend on standard_price

--- product_replenishment_cost_supplier_discount/models/product.py
+++ product_replenishment_cost_supplier_discount/models/product.py
@@ -2,21 +2,23 @@
 # Copyright 2019 PlanetaTIC - Marc Poch <mpoch@planetatic.com>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
-from odoo import models, api
+from odoo import models, api, fields
 
 
 class ProductProduct(models.Model):
     _inherit = 'product.product'
 
+    replenishment_cost = fields.Float(
+        compute='_get_replenishment_cost_supplier_discount')
+
     @api.multi
-    @api.depends('product_tmpl_id.standard_price', 'standard_price',
-                 'product_tmpl_id.seller_ids.price',
+    @api.depends('product_tmpl_id.seller_ids.price',
                  'product_tmpl_id.seller_ids.discount')
-    def _get_replenishment_cost(self):
+    def _get_replenishment_cost_supplier_discount(self):
         for product in self:
             if product.seller_ids:
                 supplierinfo = product.seller_ids[0]
                 product.replenishment_cost =\
-                    supplierinfo.price * (1.0 - supplierinfo.discount/100.0)
+                    supplierinfo.price * (1.0 - supplierinfo.discount / 100.0)
             else:
-                super(ProductProduct, product)._get_replenishment_cost()
+                product.replenishment_cost = 0.0
