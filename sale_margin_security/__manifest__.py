83c3309ae41bd9058556320fc60fafe507099127
david <david.vidal@tecnativa.com>
2020-06-01 16:43:58 +0200

    [FIX] sale_margin_security: onchange product
    In sale_margin, when the line already exists, the purchase_price
    is computed with the product onchange method, so if the user doesn't
    have the field available in the view it's not going to get the right
    price. This should be fixed in v13 with the new compute fields
    possibilities.

--- sale_margin_security/__manifest__.py
+++ sale_margin_security/__manifest__.py
@@ -2,7 +2,7 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 {
     "name": "Sale Margin Security",
-    "version": "12.0.1.0.0",
+    "version": "12.0.1.0.1",
     "author": 'Tecnativa,'
               'Odoo Community Association (OCA)',
     "website": 'https://github.com/OCA/margin-analysis',
