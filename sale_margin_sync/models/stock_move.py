6de195bce6061afa1e95a0ede0155f45dae4f418
sergio-teruel <sergio.teruel@tecnativa.com>
2020-09-17 16:38:33 +0200

    [IMP] sale_margin_sync: black, isort

--- sale_margin_sync/models/stock_move.py
+++ sale_margin_sync/models/stock_move.py
@@ -5,16 +5,18 @@ from odoo import models
 
 
 class StockMove(models.Model):
-    _inherit = 'stock.move'
+    _inherit = "stock.move"
 
     def write(self, vals):
         res = super(StockMove, self).write(vals)
-        if 'price_unit' in vals and not self.env.context.get(
-                'skip_sale_margin_sync', False):
+        if "price_unit" in vals and not self.env.context.get(
+            "skip_sale_margin_sync", False
+        ):
             self.sale_margin_sync()
         return res
 
     def sale_margin_sync(self):
-        for move in self.filtered(lambda m: (
-                m.state == 'done' and m.sale_line_id and m._is_out())):
+        for move in self.filtered(
+            lambda m: (m.state == "done" and m.sale_line_id and m._is_out())
+        ):
             move.sale_line_id.purchase_price = -move.price_unit
