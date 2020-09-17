ea853b86283eff7902bd2b59f4ef81509e8fc08b
sergio-teruel <sergio.teruel@tecnativa.com>
2020-09-17 16:38:33 +0200

    [IMP] sale_margin_sync: black, isort

--- sale_margin_sync/tests/test_sale_margin_sync.py
+++ sale_margin_sync/tests/test_sale_margin_sync.py
@@ -7,32 +7,39 @@ class TestSaleMarginSync(SavepointCase):
     @classmethod
     def setUpClass(cls):
         super().setUpClass()
-        cls.pricelist = cls.env['product.pricelist'].create({
-            'name': 'Pricelist for testing sale_margin_sync',
-        })
-        cls.partner = cls.env['res.partner'].create({
-            'name': 'Test',
-        })
-        cls.product = cls.env['product.product'].create({
-            'name': 'test_product',
-            'type': 'product',
-            'standard_price': 70,
-        })
-        cls.env['stock.quant'].create({
-            'product_id': cls.product.id,
-            'location_id': cls.env.ref('stock.stock_location_stock').id,
-            'quantity': 30.0})
-        cls.order = cls.env['sale.order'].create({
-            'partner_id': cls.partner.id,
-            'order_line': [(0, 0, {
-                'name': cls.product.name,
-                'product_id': cls.product.id,
-                'product_uom_qty': 10,
-                'product_uom': cls.product.uom_id.id,
-                'price_unit': 100.00,
-            })],
-            'pricelist_id': cls.pricelist.id,
-        })
+        cls.pricelist = cls.env["product.pricelist"].create(
+            {"name": "Pricelist for testing sale_margin_sync",}
+        )
+        cls.partner = cls.env["res.partner"].create({"name": "Test",})
+        cls.product = cls.env["product.product"].create(
+            {"name": "test_product", "type": "product", "standard_price": 70,}
+        )
+        cls.env["stock.quant"].create(
+            {
+                "product_id": cls.product.id,
+                "location_id": cls.env.ref("stock.stock_location_stock").id,
+                "quantity": 30.0,
+            }
+        )
+        cls.order = cls.env["sale.order"].create(
+            {
+                "partner_id": cls.partner.id,
+                "order_line": [
+                    (
+                        0,
+                        0,
+                        {
+                            "name": cls.product.name,
+                            "product_id": cls.product.id,
+                            "product_uom_qty": 10,
+                            "product_uom": cls.product.uom_id.id,
+                            "price_unit": 100.00,
+                        },
+                    )
+                ],
+                "pricelist_id": cls.pricelist.id,
+            }
+        )
 
     def test_sale_margin_sync(self):
         self.order.action_confirm()
