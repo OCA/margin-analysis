5865087e1245e39dcfbe1dae1edaee6d33e48c4e
oca-travis <oca+oca-travis@odoo-community.org>
2020-10-14 16:30:31 +0000

    [UPD] Update account_invoice_margin_sale.pot

---
+++ account_invoice_margin_sale/models/sale.py
@@ -0,0 +1,14 @@
+# Copyright 2017-2018 Tecnativa - Sergio Teruel
+# Copyright 2019 Tecnativa - Carlos Dauden
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+from odoo import models
+
+
+class SaleOrderLine(models.Model):
+    _inherit = "sale.order.line"
+
+    def _prepare_invoice_line(self):
+        vals = super()._prepare_invoice_line()
+        vals["purchase_price"] = self.purchase_price
+        return vals
---
+++ account_invoice_margin_sale/tests/test_account_invoice_margin_sale.py
@@ -0,0 +1,77 @@
+# Copyright 2017-2018 Tecnativa - Sergio Teruel
+# Copyright 2019 Tecnativa - Carlos Dauden
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+from odoo.tests.common import SavepointCase
+
+
+class TestAccountInvoiceMargin(SavepointCase):
+    @classmethod
+    def setUpClass(cls):
+        super(TestAccountInvoiceMargin, cls).setUpClass()
+        cls.journal = cls.env["account.journal"].create(
+            {"name": "Test journal", "type": "sale", "code": "TEST_J"}
+        )
+        cls.account_type = cls.env["account.account.type"].create(
+            {
+                "name": "Test account type",
+                "type": "receivable",
+                "internal_group": "income",
+            }
+        )
+        cls.account = cls.env["account.account"].create(
+            {
+                "name": "Test account",
+                "code": "TEST_A",
+                "user_type_id": cls.account_type.id,
+                "reconcile": True,
+            }
+        )
+        cls.partner = cls.env["res.partner"].create(
+            {"name": "Test partner", "customer_rank": 1, "is_company": True}
+        )
+        cls.partner.property_account_receivable_id = cls.account
+        cls.product_categ = cls.env["product.category"].create(
+            {"name": "Test category"}
+        )
+
+        cls.product = cls.env["product.product"].create(
+            {
+                "name": "test product",
+                "categ_id": cls.product_categ.id,
+                "uom_id": cls.env.ref("uom.product_uom_unit").id,
+                "uom_po_id": cls.env.ref("uom.product_uom_unit").id,
+                "default_code": "test-margin",
+                "invoice_policy": "order",
+                "list_price": 200.00,
+                "standard_price": 100.00,
+            }
+        )
+        cls.product.property_account_receivable_id = cls.account
+        pricelist = cls.env["product.pricelist"].create({"name": "Public Pricelist"})
+
+        cls.sale_order = cls.env["sale.order"].create(
+            {
+                "partner_id": cls.partner.id,
+                "pricelist_id": pricelist.id,
+                "order_line": [
+                    (
+                        0,
+                        0,
+                        {
+                            "name": cls.product.name,
+                            "product_id": cls.product.id,
+                            "product_uom_qty": 1,
+                            "product_uom": cls.product.uom_id.id,
+                            "price_unit": 100.0,
+                        },
+                    )
+                ],
+            }
+        )
+
+    def test_invoice_sale_order(self):
+        self.sale_order.action_confirm()
+        self.sale_order.order_line.purchase_price = 500.00
+        invoice = self.sale_order._create_invoices()
+        self.assertAlmostEqual(invoice.invoice_line_ids.purchase_price, 500.00, 2)
