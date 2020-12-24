03d6772061139030bd84a65d9c02c32b497c0c66
Mourad <mourad.elhadj.mimoune@akretion.com>
2020-12-24 10:16:40 +0100

    [IMP] add margin percent on order_line

--- sale_order_margin_percent/models/sale_order_margin.py
+++ sale_order_margin_percent/models/sale_order_margin.py
@@ -13,3 +13,19 @@ class SaleOrder(models.Model):
         for order in self:
             if order.margin and order.amount_untaxed:
                 order.percent = (order.margin / order.amount_untaxed) * 100
+
+
+class SaleOrderLine(models.Model):
+    _inherit = 'sale.order.line'
+
+    percent = fields.Float(
+        string='Percent',
+        compute='_compute_percent',
+        digits=(16, 2),
+        )
+
+    @api.depends('margin', 'price_subtotal')
+    def _compute_percent(self):
+        for order in self:
+            if order.margin and order.price_subtotal:
+                order.percent = (order.margin / order.price_subtotal) * 100
