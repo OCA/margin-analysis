52c98be3f01d7e1f4ff2e00083d1807f4a854f9f
Bhavesh Odedra <bodedra@opensourceintegrators.com>
2021-02-01 12:50:41 -0700

    [IMP] skip margin calculation for Down Payment invoice
    [UPD] Update account_invoice_margin_sale.pot
    
    [UPD] README.rst
    
    account_invoice_margin_sale 13.0.1.0.1
    
    Update translation files
    
    Updated by "Update PO files to match POT (msgmerge)" hook in Weblate.
    
    Translation: margin-analysis-13.0/margin-analysis-13.0-account_invoice_margin_sale
    Translate-URL: https://translation.odoo-community.org/projects/margin-analysis-13-0/margin-analysis-13-0-account_invoice_margin_sale/

---
+++ account_invoice_margin_sale/models/account_invoice.py
@@ -0,0 +1,26 @@
+# Copyright 2021 Sergio Teruel <sergio.teruel@tecnativa.com>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+from odoo import api, models
+
+
+class AccountMove(models.Model):
+    _inherit = "account.move"
+
+    def _get_margin_applicable_lines(self):
+        lines = super()._get_margin_applicable_lines()
+        return lines.filtered(lambda x: not x.sale_line_ids.is_downpayment)
+
+
+class AccountMoveLine(models.Model):
+    _inherit = "account.move.line"
+
+    @api.depends("purchase_price", "price_subtotal")
+    def _compute_margin(self):
+        for line in self:
+            if any(line.sale_line_ids.mapped("is_downpayment")):
+                line.update(
+                    {"margin": 0.0, "margin_signed": 0.0, "margin_percent": 0.0}
+                )
+            else:
+                super(AccountMoveLine, line)._compute_margin()
