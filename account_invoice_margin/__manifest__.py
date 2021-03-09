232191559f70579732be583d038b14a9e3d4eae0
Reyes4711 <carlos.rg.92.47@gmail.com>
2021-03-09 10:30:51 +0100

    [14.0][MIG] account_invoice_margin: Migration to 14.0

--- account_invoice_margin/__manifest__.py
+++ account_invoice_margin/__manifest__.py
@@ -1,10 +1,10 @@
 # Copyright 2017 Sergio Teruel <sergio.teruel@tecnativa.com>
-# License AGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 {
     "name": "Account Invoice Margin",
     "summary": "Show margin in invoices",
-    "version": "13.0.1.0.1",
+    "version": "14.0.1.0.0",
     "category": "Account",
     "website": "https://github.com/OCA/margin-analysis",
     "author": "Tecnativa, " "GRAP, " "Odoo Community Association (OCA)",
