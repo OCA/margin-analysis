309c25ec15e50d1b62794a47ffe57ad8a20e3ac6
Reyes4711 <carlos.rg.92.47@gmail.com>
2021-05-03 13:56:53 +0200

    [14.0][MIG] account_invoice_margin_sale: Migration to 14.0

--- account_invoice_margin/tests/__init__.py
+++
@@ -1,4 +0,0 @@
-# © 2017 Sergio Teruel <sergio.teruel@tecnativa.com>
-# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
-
-from . import test_account_invoice_margin
--- account_invoice_margin_sale/models/sale.py
+++ account_invoice_margin_sale/models/sale.py
@@ -8,7 +8,7 @@ from odoo import models
 class SaleOrderLine(models.Model):
     _inherit = "sale.order.line"
 
-    def _prepare_invoice_line(self):
-        vals = super()._prepare_invoice_line()
+    def _prepare_invoice_line(self, **optional_values):
+        vals = super()._prepare_invoice_line(**optional_values)
         vals["purchase_price"] = self.purchase_price
         return vals
