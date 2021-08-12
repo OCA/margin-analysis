1736ea19afa1aa46a7ddc9ce561b5c183e2081d6
Bhavesh Odedra <bodedra@opensourceintegrators.com>
2021-08-12 16:01:33 -0700

    [ENH] account_invoice_margin: add 'Margin(%)' field on Invoice analysis report

--- account_invoice_margin/report/account_invoice_report.py
+++ account_invoice_margin/report/account_invoice_report.py
@@ -8,11 +8,13 @@ class AccountInvoiceReport(models.Model):
     _inherit = "account.invoice.report"
 
     margin = fields.Float(string='Margin', readonly=True)
+    margin_percent = fields.Float(string='Margin (%)', readonly=True)
 
     def _select(self):
         select_str = super()._select()
-        return '%s, sub.margin' % select_str
+        return '%s, sub.margin, sub.margin_percent' % select_str
 
     def _sub_select(self):
         select_str = super()._sub_select()
-        return '%s, SUM(ail.margin_signed) AS margin' % select_str
+        return '%s, SUM(ail.margin_signed) AS margin, ' \
+               'AVG(ail.margin_percent) AS margin_percent' % select_str
