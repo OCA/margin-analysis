210964db90a940f45dcf3372d472a6e887355f32
Víctor Martínez <victor.martinez@tecnativa.com>
2021-08-18 08:29:25 +0200

    [IMP] account_invoice_margin: Add Cost price measure to Invoices Analysis

--- account_invoice_margin/report/account_invoice_report.py
+++ account_invoice_margin/report/account_invoice_report.py
@@ -1,4 +1,5 @@
 # © 2017 Sergio Teruel <sergio.teruel@tecnativa.com>
+# Copyright 2021 Tecnativa - Víctor Martínez
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 from odoo import fields, models
@@ -7,8 +8,12 @@ from odoo import fields, models
 class AccountInvoiceReport(models.Model):
     _inherit = "account.invoice.report"
 
+    purchase_price = fields.Float(string="Cost Price", readonly=True)
     margin = fields.Float(string="Margin", readonly=True)
 
     def _select(self):
         select_str = super()._select()
-        return "%s, SUM(line.margin_signed) AS margin" % select_str
+        return (
+            "%s, SUM(line.purchase_price) AS purchase_price, "
+            "SUM(line.margin_signed) AS margin" % select_str
+        )
