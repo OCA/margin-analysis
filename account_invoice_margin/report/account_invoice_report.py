824fcd15cb152c67451d2564857793db69744a82
Hardik Suthar <hsuthar@opensourceintegrators.com>
2021-08-20 15:20:44 +0530

    [IMP] account_invoice_margin: groupby margin(%) calculation

--- account_invoice_margin/report/account_invoice_report.py
+++ account_invoice_margin/report/account_invoice_report.py
@@ -1,7 +1,7 @@
 # © 2017 Sergio Teruel <sergio.teruel@tecnativa.com>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
-from odoo import fields, models
+from odoo import api, fields, models
 
 
 class AccountInvoiceReport(models.Model):
@@ -16,3 +16,30 @@ class AccountInvoiceReport(models.Model):
             "%s, SUM(line.margin_signed) AS margin, "
             "AVG(move.margin_percent)  AS margin_percent" % select_str
         )
+
+    @api.model
+    def read_group(
+        self, domain, fields, groupby, offset=0, limit=None, orderby=False, lazy=True
+    ):
+        res = super(AccountInvoiceReport, self).read_group(
+            domain,
+            fields,
+            groupby,
+            offset=offset,
+            limit=limit,
+            orderby=orderby,
+            lazy=lazy,
+        )
+        if "margin" not in fields:
+            return res
+
+        full_fields = all(x in fields for x in {"margin", "margin_percent"})
+        margin_percent = "margin_percent" in fields
+        for line in res:
+            if full_fields:  # compute difference
+                line["margin_percent"] = (
+                    (line["margin"] or 0.0) / line["price_total"]
+                ) * 100
+            elif margin_percent:  # Remove wrong 0 values
+                del line["margin_percent"]
+        return res
