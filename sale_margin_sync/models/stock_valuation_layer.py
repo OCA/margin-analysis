d155a7c9971b74e43281442711ee2308b039d3f5
sergio-teruel <sergio.teruel@tecnativa.com>
2021-08-30 17:52:47 +0200

    [IMP] sale_margin_sync: Sync sale line purchase price from stock valuation layer unit cost TT31421

--- sale_margin_sync/models/stock_valuation_layer.py
+++ sale_margin_sync/models/stock_valuation_layer.py
@@ -1,11 +1,20 @@
 # Copyright 2019 Tecnativa - Carlos Dauden
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 
-from odoo import models
+from odoo import api, models
 
 
 class StockValuationLayer(models.Model):
     _inherit = "stock.valuation.layer"
+
+    @api.model_create_multi
+    def create(self, vals_list):
+        """Update purchase price from sale line linked to svl record
+        """
+        svls = super().create(vals_list)
+        if not self.env.context.get("skip_sale_margin_sync", False):
+            svls.filtered("stock_move_id.sale_line_id").sale_margin_sync()
+        return svls
 
     def write(self, vals):
         res = super().write(vals)
@@ -16,5 +25,7 @@ class StockValuationLayer(models.Model):
         return res
 
     def sale_margin_sync(self):
+        """Only synchronize outgoing moves
+        """
         for svl in self.filtered(lambda l: (l.quantity < 0.0)):
             svl.stock_move_id.sale_line_id.purchase_price = svl.unit_cost
