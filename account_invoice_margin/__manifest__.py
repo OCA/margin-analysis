2e00e65a0c88697b850d0bdc8559aa348ff5af38
Luis Escobar <lescobar@vauxoo.com>
2021-09-21 20:24:38 +0000

    [IMP] account_invoice_margin: Avoid error when trying to install module with database populated with many invoices
        Currently, when the module is installed in a database populated with
        invoices, the compute methods are executed when fields are created,
        if the number of records is not many, the fields are set correctly,
        the problem lies when the records of these invoice lines are a
        considerable count (for example > 400 thousand AML records) does not
        finish running the compute methods and the odoo server closes
        unexpectedly, causing the module to fail to install.
    
        In order to solve this problem, it was required to create a hook to
        create the fields to avoid the execution of the computed methods
        and initialize margin percent to 100% to avoid inconsistency, so
        that in this way the installation of the module culminates successfully.

--- account_invoice_margin/__manifest__.py
+++ account_invoice_margin/__manifest__.py
@@ -4,7 +4,7 @@
 {
     "name": "Account Invoice Margin",
     "summary": "Show margin in invoices",
-    "version": "13.0.1.2.0",
+    "version": "13.0.1.2.1",
     "category": "Account",
     "website": "https://www.github.com/OCA/margin-analysis",
     "author": "Tecnativa, " "GRAP, " "Odoo Community Association (OCA)",
@@ -18,4 +18,5 @@
         "security/account_invoice_margin_security.xml",
         "views/account_invoice_margin_view.xml",
     ],
+    "pre_init_hook": "pre_init_hook",
 }
