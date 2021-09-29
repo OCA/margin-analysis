917af84e9cf2e172fb1f1efa9587feec4481ef6b
ferran-73 <ferran@studio73.es>
2021-09-29 17:19:36 +0200

    [14.0][MIG] saler_report_margin: Migration to V14

--- sale_report_margin/__manifest__.py
+++ sale_report_margin/__manifest__.py
@@ -2,8 +2,9 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 {
     "name": "Sale Report Margin",
-    "version": "13.0.1.0.0",
+    "version": "14.0.1.0.0",
     "author": "Tecnativa," "Odoo Community Association (OCA)",
+    "summary": "Adds the product purchase price to sale analysis report",
     "website": "https://github.com/OCA/margin-analysis",
     "development_status": "Production/Stable",
     "maintainers": ["sergio-teruel"],
