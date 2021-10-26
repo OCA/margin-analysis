e51085b2b00ccdcd3235ff660f8de27d943a2824
david <david.vidal@tecnativa.com>
2021-10-26 16:02:02 +0200

    [IMP] account_invoice_margin_sale_delivered_sync: black, isort, prettier

--- account_invoice_margin_sale_delivered_sync/models/sale.py
+++ account_invoice_margin_sale_delivered_sync/models/sale.py
@@ -5,10 +5,9 @@ from odoo import models
 
 
 class SaleOrderLine(models.Model):
-    _inherit = 'sale.order.line'
+    _inherit = "sale.order.line"
 
     def _prepare_invoice_line(self, qty):
         vals = super()._prepare_invoice_line(qty)
-        vals['purchase_price'] = (self.purchase_price_delivery or
-                                  self.purchase_price)
+        vals["purchase_price"] = self.purchase_price_delivery or self.purchase_price
         return vals
