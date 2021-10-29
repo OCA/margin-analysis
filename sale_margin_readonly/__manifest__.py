27095433e8ba6eb1226d09223b8b30dbbbff5850
Ivàn Todorovich <ivan.todorovich@gmail.com>
2021-10-29 12:30:46 -0300

    [ADD] sale_margin_readonly

---
+++ sale_margin_readonly/__manifest__.py
@@ -0,0 +1,16 @@
+# Copyright 2021 Moka Tourisme (https://www.mokatourisme.fr).
+# @author Iván Todorovich <ivan.todorovich@gmail.com>
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+
+{
+    "name": "Sale Margin Readonly",
+    "summary": "Sale Margin Readonly",
+    "version": "12.0.1.0.0",
+    "author": "Moka Tourisme, Odoo Community Association (OCA)",
+    "maintainers": ["ivantodorovich"],
+    "website": "https://github.com/OCA/margin-analysis",
+    "license": "AGPL-3",
+    "category": "Sales",
+    "depends": ["sale_margin"],
+    "data": ["views/sale_order.xml"],
+}
---
+++ sale_margin_readonly/readme/DESCRIPTION.rst
@@ -0,0 +1,1 @@
+Prevents the user from modifying the `purchase_price` (Cost) field on Sales Order Lines.
