dbf94d773c8eac4005fbd2743ad32f6bbad9f303
Pedro M. Baeza <pedro.baeza@tecnativa.com>
2022-07-05 00:14:58 +0200

    Update dotfiles + switch to GH actions

--- sale_order_margin_percent/__manifest__.py
+++ sale_order_margin_percent/__manifest__.py
@@ -6,7 +6,7 @@
     "summary": "Show Percent in sale order",
     "version": "13.0.1.1.0",
     "category": "Sales",
-    "website": "https://github.com/OCA/margin-analysis/",
+    "website": "https://github.com/OCA/margin-analysis",
     "author": "PESOL, Odoo Community Association (OCA)",
     "license": "AGPL-3",
     "installable": True,
