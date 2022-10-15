bbac7e4eec7c35ea854f2d9843ee6f4d8d1912a8
rafamar <rmartinez@puntsistemes.es>
2022-10-15 23:48:12 +0200

    [MIG] sale_margin_sync: Migration to 14.0

--- sale_margin_sync/models/stock_valuation_layer.py
+++ sale_margin_sync/models/stock_valuation_layer.py
@@ -26,4 +26,11 @@ class StockValuationLayer(models.Model):
     def sale_margin_sync(self):
         """Only synchronize outgoing moves"""
         for svl in self.filtered(lambda l: (l.quantity < 0.0)):
-            svl.stock_move_id.sale_line_id.purchase_price = svl.unit_cost
+            sale_line_id = svl.stock_move_id.sale_line_id
+            product_cost = svl.unit_cost
+            if sale_line_id.product_uom and sale_line_id.product_uom != svl.uom_id:
+                product_cost = svl.uom_id._compute_price(
+                    product_cost,
+                    sale_line_id.product_uom,
+                )
+            sale_line_id.purchase_price = product_cost
