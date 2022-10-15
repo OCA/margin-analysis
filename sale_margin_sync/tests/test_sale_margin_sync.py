ce153b3910d9677f8a957681574f1cc759a8157d
rafamar <rmartinez@puntsistemes.es>
2022-10-15 23:48:12 +0200

    [MIG] sale_margin_sync: Migration to 14.0

--- sale_margin_sync/tests/test_sale_margin_sync.py
+++ sale_margin_sync/tests/test_sale_margin_sync.py
@@ -35,7 +35,18 @@ class TestSaleMarginSync(SavepointCase):
                             "product_uom": cls.product.uom_id.id,
                             "price_unit": 100.00,
                         },
-                    )
+                    ),
+                    (
+                        0,
+                        0,
+                        {
+                            "name": cls.product.name,
+                            "product_id": cls.product.id,
+                            "product_uom_qty": 2,
+                            "product_uom": cls.env.ref("uom.product_uom_dozen").id,
+                            "price_unit": 1200.00,
+                        },
+                    ),
                 ],
                 "pricelist_id": cls.pricelist.id,
             }
@@ -43,19 +54,31 @@ class TestSaleMarginSync(SavepointCase):
 
     def test_sale_margin_sync(self):
         self.order.action_confirm()
-        so_line = self.order.order_line[:1]
-        move = so_line.move_ids[:1]
-        move.quantity_done = 10
-        move.picking_id.action_done()
-        move.stock_valuation_layer_ids[:1].unit_cost = 80.0
-        self.assertEqual(so_line.purchase_price, 80.0)
-        self.assertEqual(so_line.margin, 200.0)
+        so_line1 = self.order.order_line[:1]
+        move1 = so_line1.move_ids[:1]
+        move1.quantity_done = 10
+        so_line2 = self.order.order_line[1:2]
+        move2 = so_line2.move_ids[:1]
+        move2.quantity_done = 2
+        self.order.picking_ids[:1]._action_done()
+        move1.stock_valuation_layer_ids[:1].unit_cost = 80.0
+        move2.stock_valuation_layer_ids[:1].unit_cost = 80.0
+        self.assertEqual(so_line1.purchase_price, 80.0)
+        self.assertEqual(so_line1.margin, 200.0)
+        self.assertEqual(so_line2.purchase_price, 960.0)
+        self.assertEqual(so_line2.margin, 480)
 
     def test_sale_margin_sync_unvalidated_move(self):
         self.order.action_confirm()
-        so_line = self.order.order_line[:1]
-        move = so_line.move_ids[:1]
-        move.quantity_done = 10
-        move.stock_valuation_layer_ids[:1].unit_cost = 80.0
-        self.assertEqual(so_line.purchase_price, 70.0)
-        self.assertEqual(so_line.margin, 300.0)
+        so_line1 = self.order.order_line[:1]
+        move1 = so_line1.move_ids[:1]
+        move1.quantity_done = 10
+        move1.stock_valuation_layer_ids[:1].unit_cost = 80.0
+        so_line2 = self.order.order_line[1:2]
+        move2 = so_line2.move_ids[:1]
+        move2.quantity_done = 2
+        move2.stock_valuation_layer_ids[:1].unit_cost = 80.0
+        self.assertEqual(so_line1.purchase_price, 70.0)
+        self.assertEqual(so_line1.margin, 300.0)
+        self.assertEqual(so_line2.purchase_price, 840.0)
+        self.assertEqual(so_line2.margin, 720)
