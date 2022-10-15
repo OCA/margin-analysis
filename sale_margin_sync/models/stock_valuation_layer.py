dc46ae21afb8850c535b201b35d63015d5f593c2
rafamar <rmartinez@puntsistemes.es>
2022-10-15 23:00:51 +0200

    [IMP] sale_margin_sync: black, isort, prettier

--- sale_margin_sync/models/stock_valuation_layer.py
+++ sale_margin_sync/models/stock_valuation_layer.py
@@ -9,8 +9,7 @@ class StockValuationLayer(models.Model):
 
     @api.model_create_multi
     def create(self, vals_list):
-        """Update purchase price from sale line linked to svl record
-        """
+        """Update purchase price from sale line linked to svl record"""
         svls = super().create(vals_list)
         if not self.env.context.get("skip_sale_margin_sync", False):
             svls.filtered("stock_move_id.sale_line_id").sale_margin_sync()
@@ -25,7 +24,6 @@ class StockValuationLayer(models.Model):
         return res
 
     def sale_margin_sync(self):
-        """Only synchronize outgoing moves
-        """
+        """Only synchronize outgoing moves"""
         for svl in self.filtered(lambda l: (l.quantity < 0.0)):
             svl.stock_move_id.sale_line_id.purchase_price = svl.unit_cost
