4075ec6304bfe3b1e22fa3aca68c32388555b24e
Álvaro <alvaro.marcos@factorlibre.com>
2023-06-05 12:23:09 +0200

    [IMP] product_replenishment_cost: pre-commit stuff

--- product_replenishment_cost/__manifest__.py
+++ product_replenishment_cost/__manifest__.py
@@ -3,19 +3,19 @@
 # @author: Alexandre Fayolle
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 {
-    'name': 'Product Replenishment Cost',
-    'summary': "Provides an overridable method on product which compute"
-               "the Replenishment cost of a product",
-    'version': '12.0.1.0.1',
-    'author': "Camptocamp,GRAP,Odoo Community Association (OCA)",
-    'license': 'AGPL-3',
-    'category': 'Products',
-    'depends': [
-        'product',
+    "name": "Product Replenishment Cost",
+    "summary": "Provides an overridable method on product which compute"
+    "the Replenishment cost of a product",
+    "version": "12.0.1.0.1",
+    "author": "Camptocamp,GRAP,Odoo Community Association (OCA)",
+    "license": "AGPL-3",
+    "category": "Products",
+    "depends": [
+        "product",
     ],
-    'website': 'https://github.com/OCA/margin-analysis',
-    'data': [
-        'views/product_view.xml',
+    "website": "https://github.com/OCA/margin-analysis",
+    "data": [
+        "views/product_view.xml",
     ],
-    'installable': True,
+    "installable": True,
 }
