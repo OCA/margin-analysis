05fcaaa12979fe5766a8655b5fca459c2a9171c3
Álvaro <alvaro.marcos@factorlibre.com>
2023-06-05 12:59:19 +0200

    [MIG] product_replenishment_cost: Migration to 16.0

--- product_replenishment_cost/tests/test_module.py
+++ product_replenishment_cost/tests/test_module.py
@@ -2,13 +2,14 @@
 # @author: Sylvain LE GAL (https://twitter.com/legalsylvain)
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
-from openerp.tests.common import TransactionCase
+from odoo.tests.common import TransactionCase
 
 
 class TestModule(TransactionCase):
-    def setUp(self):
-        super(TestModule, self).setUp()
-        self.ProductProduct = self.env["product.product"]
+    @classmethod
+    def setUpClass(cls):
+        super().setUpClass()
+        cls.ProductProduct = cls.env["product.product"]
 
     # Test Section
     def test_create_or_update(self):
