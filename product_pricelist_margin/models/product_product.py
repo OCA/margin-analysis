039ebcf3d3ddb2edca259414310886791ad0b018
Hugo <hugo.cordoba@factorlibre.com>
2023-06-05 16:54:34 +0200

    [MIG] product_pricelist_margin: Migration to 16.0

--- product_pricelist_margin/models/product_product.py
+++ product_pricelist_margin/models/product_product.py
@@ -2,13 +2,12 @@
 # @author: Sylvain LE GAL (https://twitter.com/legalsylvain)
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
-from odoo import _, api, models
+from odoo import models
 
 
 class Productproduct(models.Model):
     _inherit = "product.product"
 
-    @api.multi
     def button_margin_per_pricelist(self):
         self.ensure_one()
         view = self.env.ref(
--- product_pricelist_margin/models/product_template.py
+++ product_pricelist_margin/models/product_template.py
@@ -2,13 +2,12 @@
 # @author: Sylvain LE GAL (https://twitter.com/legalsylvain)
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
-from odoo import api, models
+from odoo import models
 
 
 class ProductTemplate(models.Model):
     _inherit = "product.template"
 
-    @api.multi
     def button_margin_per_pricelist(self):
         self.ensure_one()
         return self.mapped("product_variant_ids")[0].button_margin_per_pricelist()
