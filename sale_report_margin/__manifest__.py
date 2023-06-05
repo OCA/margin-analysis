ee7957b2f2cd4a25fb51632fde6b43b4a6ba8fd4
Alex Guardado <alex.guardado@factorlibre.com>
2023-06-05 13:01:58 +0200

    [MIG] sale_report_margin: Migration to 16.0

--- sale_report_margin/__manifest__.py
+++ sale_report_margin/__manifest__.py
@@ -2,7 +2,7 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 {
     "name": "Sale Report Margin",
-    "version": "15.0.1.0.0",
+    "version": "16.0.1.0.0",
     "author": "Tecnativa, Odoo Community Association (OCA)",
     "website": "https://github.com/OCA/margin-analysis",
     "development_status": "Production/Stable",
