8551f99a889d904ec7da3f9f0e9de4f61885e6f1
Sylvain LE GAL <sylvain.legal@grap.coop>
2023-09-13 20:49:58 +0200

    [REF] split sale_margin_delivered into sale_margin_delivered and sale_margin_delivered_security

---
+++ sale_margin_delivered_security/__manifest__.py
@@ -0,0 +1,16 @@
+# Copyright 2018 Tecnativa - Sergio Teruel
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+{
+    "name": "Sale Margin Delivered - Sale Margin Security",
+    "summary": "Glue module between sale margin delivered"
+    " and sale margin security modules",
+    "version": "16.0.1.0.0",
+    "author": "Tecnativa, GRAP, Odoo Community Association (OCA)",
+    "website": "https://github.com/OCA/margin-analysis",
+    "category": "Sales",
+    "license": "AGPL-3",
+    "depends": ["sale_margin_delivered", "sale_margin_security"],
+    "data": ["views/sale_margin_delivered_view.xml"],
+    "auto_install": True,
+    "maintainers": ["sergio-teruel"],
+}
