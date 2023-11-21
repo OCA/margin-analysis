7c9e8eb952a62d8e269dcd7c8bbadca12bda3d29
Carlos Dauden <carlos.dauden@tecnativa.com>
2023-11-21 17:34:17 +0100

    [IMP] sale_margin_security: Avoid display margin in measures pivot view. Set group_sale_margin_security in all sensible fields.

--- sale_margin_security/report/sale_report.py
+++ sale_margin_security/report/sale_report.py
@@ -1,9 +1,10 @@
+# Copyright 2023 Carlos Dauden - Tecnativa
+# Copyright 2023 Sergio Teruel - Tecnativa
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 from odoo import fields, models
 
 
 class SaleReport(models.Model):
     _inherit = "sale.report"
 
-    margin = fields.Float(
-        groups="sale_margin_security.group_sale_margin_security",
-    )
+    margin = fields.Float(groups="sale_margin_security.group_sale_margin_security")
