ff0f45fd48b718851c1461248ffcf0d8b8b6be53
Eduardo De Miguel <edu@moduon.team>
2024-06-18 14:01:19 +0200

    [ADD] sale_margin_delivered_dropshipping: Compute delivered margins on Dropshipping

---
+++ sale_margin_delivered_dropshipping/__manifest__.py
@@ -0,0 +1,16 @@
+# Copyright 2024 Moduon Team
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+{
+    "name": "Sale Margin Delivered Dropshipping",
+    "version": "16.0.1.0.1",
+    "author": "Moduon, Odoo Community Association (OCA)",
+    "website": "https://github.com/OCA/margin-analysis",
+    "category": "Sales",
+    "license": "AGPL-3",
+    "depends": ["sale_margin_delivered", "stock_dropshipping"],
+    "data": [],
+    "installable": True,
+    "auto_install": True,
+    "development_status": "Alpha",
+    "maintainers": ["Shide"],
+}
