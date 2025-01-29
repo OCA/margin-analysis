e81c703355eaea8ca432dd1741840973f186c3fe
Carlos Lopez <carlos.lopez@tecnativa.com>
2025-01-29 06:41:12 -0500

    [MIG] sale_margin_security: Migration to version 17.0

--- sale_margin_security/__manifest__.py
+++ sale_margin_security/__manifest__.py
@@ -2,7 +2,7 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 {
     "name": "Sale Margin Security",
-    "version": "16.0.3.1.0",
+    "version": "17.0.1.0.0",
     "author": "Tecnativa," "Odoo Community Association (OCA)",
     "website": "https://github.com/OCA/margin-analysis",
     "category": "Sales",
