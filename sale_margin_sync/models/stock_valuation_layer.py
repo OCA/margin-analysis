dc9789b05b7b8545e329584add8a6d7a8cc81f4d
Bhavesh Heliconia <oca@heliconia.in>
2025-02-17 15:24:16 +0530

    [IMP] sale_margin_sync: pre-commit auto fixes

--- sale_margin_sync/models/stock_valuation_layer.py
+++ sale_margin_sync/models/stock_valuation_layer.py
@@ -25,7 +25,7 @@ class StockValuationLayer(models.Model):
 
     def sale_margin_sync(self):
         """Only synchronize outgoing moves"""
-        for svl in self.filtered(lambda l: (l.quantity < 0.0)):
+        for svl in self.filtered(lambda layer: (layer.quantity < 0.0)):
             sale_line_id = svl.stock_move_id.sale_line_id
             product_cost = svl.unit_cost
             if sale_line_id.product_uom and sale_line_id.product_uom != svl.uom_id:
