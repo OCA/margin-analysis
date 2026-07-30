c895a6c128698ebab1357a405b30736f8bb40eff
Alex Cuellar <acuellar@grupoyacck.com>
2026-07-30 17:57:55 -0500

    [MIG] product_replenishment_cost: Migration to 19.0

--- product_replenishment_cost/__manifest__.py
+++ product_replenishment_cost/__manifest__.py
@@ -6,7 +6,7 @@
     "name": "Product Replenishment Cost",
     "summary": "Provides an overridable method on product which compute"
     "the Replenishment cost of a product",
-    "version": "18.0.1.0.0",
+    "version": "19.0.1.0.0",
     "author": "Camptocamp,GRAP,Odoo Community Association (OCA)",
     "license": "AGPL-3",
     "category": "Products",
